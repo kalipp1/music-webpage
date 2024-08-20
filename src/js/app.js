6a3b46f0febcb1c330ff5d59f11cb6c38f20911c
Maks Birecki <maksymilian.birecki@gmail.com>
2024-08-20 23:24:10 +0200

    activatePageScriptAdded

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,40 @@
 import {select, classNames} from './settings.js';
 const app = {
     initPages: function(){
-      const thisApp = this;
-      thisApp.pages = document.querySelector(select.containerOf.pages).children;
-
-      thisApp.navLinks = document.querySelectorAll(select.nav.links);
-
-      const idFromHash = window.location.hash.replace('#/', '');
-
-      let pageMatchingHash = thisApp.pages[0].id;
-
-      for(let page of thisApp.pages){
-        if(page.id == idFromHash){
-          pageMatchingHash = page.id;
-          break;
+        const thisApp = this;
+        thisApp.pages = document.querySelector(select.containerOf.pages).children;
+  
+        thisApp.navLinks = document.querySelectorAll(select.nav.links);
+  
+        const idFromHash = window.location.hash.replace('#/', '');
+  
+        let pageMatchingHash = thisApp.pages[0].id;
+  
+        for(let page of thisApp.pages){
+          if(page.id == idFromHash){
+            pageMatchingHash = page.id;
+            break;
+          }
         }
-      console.log(pageMatchingHash);
-      thisApp.activatePage(pageMatchingHash);
+  
+        console.log(pageMatchingHash);
+        thisApp.activatePage(pageMatchingHash);
+  
+        for(let link of thisApp.navLinks){
+          link.addEventListener('click', function(event){
+            const clickedElement = this;
+            event.preventDefault();
+  
+            // get page id from href attribute
+            const id = clickedElement.getAttribute('href').replace('#', '');
+  
+            // change URL hash
+            window.location.hash = '#/' + id;
 
-      for(let link of thisApp.navLinks){
-        link.addEventListener('click', function(event){
-          const clickedElement = this;
-          event.preventDefault();
-
-          // get page id from href attribute
-          const id = clickedElement.getAttribute('href').replace('#', '');
-
-          // run thisApp.activatePage with that id
-          thisApp.activatePage(id);
-          // change URL hash
-          window.location.hash = '#/' + id;
-        });
+            thisApp.activatePage(id);
+          });
         }
-        }
-    },
+      },
     activatePage: function(pageId){
         const thisApp = this;
         // add class "active" to matching pages. remove from non-matching
